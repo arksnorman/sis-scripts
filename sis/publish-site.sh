aa8ce97af0de6c52459ff82d736743ddfc2f6806
Kidhoma Norman <arksnorman@gmail.com>
2018-03-29 05:15:58 +0300

    Add sis SH scripts

---
+++ sis/publish-site.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+siteName=$2
+branchName=$1
+
+# Define the variables with values you want replaced
+serverName='$branchName.$siteName'
+serverAlias='www.$branchName.$siteName'
+documentRoot='/home/www/sis/$branchName/public'
+$templatePath='/etc/apache2/sites-available/sis-vhost.template'
+
+$file='/etc/apache2/sites-available/$serverName.conf'
+
+if [ ! -f "$templatePath" ]; then
+	echo "Apache template file not found"
+	exit 1
+fi
+
+# This could also be read in via bash arguments. 
+# Google "bash getopts" for more information
+# render a template configuration file
+# expand variables + preserve formatting
+# user="Venkatt"
+# referenced inside the template.txt as $user
+# render_template /path/to/template.txt > path/to/configuration_file
+function renderTemplate() {
+    eval "echo \"$(cat $1)\""
+}
+
+if [[ ! renderTemplate $template > $file ]]; then
+	echo "Failed to create file template"
+	exit 1
+fi
