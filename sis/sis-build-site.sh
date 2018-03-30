d2a0bf951f2ce380df79ca583a13a946fadc7aef
Kidhoma Norman <arksnorman@gmail.com>
2018-03-30 03:45:44 +0300

    fix repo url

--- sis/sis-build-site.sh
+++ sis/sis-build-site.sh
@@ -9,7 +9,7 @@ cleanBranchName=$(echo $1 | sed -e 's/\.//g')
 fullDir="$baseDir$cleanBranchName"
 
 if [ ! -d "$fullDir" ]; then
-    mkdir -p "$fullDir" && cd "$fullDir" && git clone "repositoryUrl" .
+    mkdir -p "$fullDir" && cd "$fullDir" && git clone "$repositoryUrl" .
 fi
 
 cd ${fullDir} && git reset --hard && git checkout ${branchName} && git pull
