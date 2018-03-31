cc0a6639bd488956c900bfbed7de543e97812811
Kidhoma Norman <arksnorman@gmail.com>
2018-03-31 23:04:17 +0300

    copy static files to public folder

--- sis/sis-configure-site.sh
+++ sis/sis-configure-site.sh
@@ -4,5 +4,6 @@ branchName=$(echo $1 | sed -e 's/\.//g')
 branchPath="/home/www/sis/$branchName"
 
 cp /home/www/data.ini ${branchPath}
+cp -r /home/www/static "$branchPath/public/"
 chown -R www-data:www-data ${branchPath}
 chmod -R 775 ${branchPath}
