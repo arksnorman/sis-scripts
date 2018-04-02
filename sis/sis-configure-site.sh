448d55da97e31c076f07231268e495f1310335ba
Kidhoma Norman <arksnorman@gmail.com>
2018-04-02 05:23:28 +0300

    Fix previous commit. Problem was in dependencies not configure-sites

--- sis/sis-configure-site.sh
+++ sis/sis-configure-site.sh
@@ -4,7 +4,6 @@ branchName=$(echo $1 | sed -e 's/\.//g')
 branchPath="/home/www/sis/$branchName"
 
 cp /home/www/data.ini ${branchPath}
-cp /home/www/parameters.yaml "$branchPath/config/"
 cp -r /home/www/static "$branchPath/public/"
 chown -R www-data:www-data ${branchPath}
 chmod -R 775 ${branchPath}
--- sis/sis-install-dependencies.sh
+++ sis/sis-install-dependencies.sh
@@ -3,4 +3,5 @@
 branchName=$(echo $1 | sed -e 's/\.//g')
 branchPath="/home/www/sis/$branchName"
 
+cp /home/www/parameters.yaml "$branchPath/config/"
 cd ${branchPath} && composer update
